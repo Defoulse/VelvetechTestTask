b7aa002ffb5d054a7b3a6362f6e6025b60e3bd15
Defoulse <aidarkhan3500@gmail.com>
2022-08-06 16:44:08 +0600

    Add: Create method requirements

--- Dtos/TodoItemCreateDto.cs
+++ Dtos/TodoItemCreateDto.cs
@@ -4,8 +4,12 @@ namespace TodoApi.Dtos
 {
     public class TodoItemCreateDto
     {
+        [Required]
+        [MaxLength(250)]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public bool IsComplete { get; set; }
     }
 }
